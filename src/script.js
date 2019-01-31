ece006834ed5b54f397dab52c32a16169a8835e5
Sierra Morgan <sierramorgan07@gmail.com>
2019-01-31 13:56:54 -0500

    adding functions for functionality

--- src/script.js
+++ src/script.js
@@ -1,9 +1,40 @@
 
-// var button = document.getElementById('gogo');
+// PopUp Error Messaging Handler
 
-// button.onclick = gogo(){
-// 	window.open('vid.html');
-// }
+var box = document.querySelector(".popUp");
+
+document.addEventListener("click", function(event) {
+
+	if (event.target.closest(".popUp")) return;
+
+	box.classList.add("hidden");
+});
+
+
+// Inputs to Outputs
+
+var output = document.querySelector(".output");
+var input = document.querySelector("input").value;
+var button = document.querySelector("button");
+
+var regex = /[A-Z]/g;
+// var found = input.match(regex);
+
+button.onClick = gogo();
+
+function gogo() { 
+	if (input.match(regex)) {
+		location.href = "vid.html";
+		console.log("yas");
+	}
+	else {
+		box.classList.add("display");
+		console.log("nope");
+	}
+	
+}
+
+
 
 
 
